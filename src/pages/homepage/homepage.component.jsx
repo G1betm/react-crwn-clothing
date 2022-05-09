cadd89b03ddf7877e1ef1a33dc6b881f7294057d
g1be <example@email.com>
2022-05-09 17:50:39 +0500

    added collection preview and collection preview item with data and styles

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import './homepage.styles.scss';
 import Directory from "../../components/directory/directory.component";
 
-const HomePage = ({history}) => {
+const HomePage = () => {
 	return (
 		<div className='homepage'>
 			<Directory />
